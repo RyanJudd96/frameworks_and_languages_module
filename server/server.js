eb8c381716904743f55bfc003d66267e876845b0
RyanJudd96 <96239336+RyanJudd96@users.noreply.github.com>
2023-10-25 22:07:02 +0000

    worked on final get request

--- server/server.js
+++ server/server.js
@@ -22,6 +22,21 @@ let items =
     "lon": 1.0830275,
     "date_from": "2023-10-23T13:12:39.844Z",
     "date_to": "2023-10-23T13:12:39.844Z"
+  },
+  {
+    "id": 1,
+    "user_id": "user3000",
+    "keywords": [
+      "hammer",
+      "nails",
+      "tools"
+    ],
+    "description": "A hammer and nails set",
+    "image": "https://placekitten.com/200/300",
+    "lat": 51.2798438,
+    "lon": 1.0830275,
+    "date_from": "2023-10-23T13:12:39.844Z",
+    "date_to": "2023-10-23T13:12:39.844Z"
   }
 ]
 //#endregion
@@ -33,20 +48,25 @@ app.listen(port, () => {
 })
 
 app.get('/', (req, res) => {
+  //posts basic text to the webpage to confirm success of server initialization
   res.send('Server build successful!')
   res.status(200)
 })
 
 app.post('/item', (req, res) => {
+
   if(Object.keys(req.body).toString() !="user_id,keywords,description,lat,lon")
   {
+    //checks entered data is correct and returns error code and message in case of failure
     return res.status(405).json({"message": "missing fields"})
   }
   else
   {
+    //generates current date and random id number
     const currentDate = new Date().toISOString();
     const identifier = Math.random();
 
+    //creates a new field
     req.body.id= identifier;
     req.body['date_from'] = currentDate;
     req.body['id'] = identifier;
@@ -59,12 +79,29 @@ app.post('/item', (req, res) => {
 
 app.get('/items', (req, res) => {
   res.json(items)
-  res.status(200).json("Message: ", "Successful GET requests")
+  es.status(200).json("Message: ", "Successful GET requests")
 })
 
-app.get('/item/', (req, res) => {
-  res.json(items[id])
+app.get('/item/:id', (req, res) => {
+
+  const id = parseFloat(req.params.id);
+
+  const searchedItem = items.find(searchedItem => searchedItem.id === id);
+
+  if(!searchedItem)
+  {
+    console.log("Error 404");
+    res.status(404).json('Search Failed');
+  }
+
+  console.log("User ID found!" + searchedItem);
+  res.status(200).json(searchedItem);
+  return;  
 })
+
+//app.delete('/user', (req, res) => {
+ // res.send('Got a DELETE request at /user')
+//})
 
 //#endregion
 
